83f54cbb1d32c2e16d6d3e8e4bc6ba02630bafad
Vasga <vasga@ro.ru>
2022-04-28 18:03:23 +0300

    PY110 21/04 2022

--- Занятие1/Практические_задания/task4_2/main.py
+++ Занятие1/Практические_задания/task4_2/main.py
@@ -5,7 +5,7 @@ def task():
     iterator_numbers = count(1, 1)
     # numbers = [i ** 2 for i in iterator_numbers if i % 2 == 0]  # TODO заменить на map и filter
     numbers = map(lambda i: i ** 2, filter(lambda i: i % 2 == 0, iterator_numbers))
-    for num in range(10):  # TODO напечатать первые 10 чисел
+    for _ in range(10):  # TODO напечатать первые 10 чисел
         print(next(numbers))  # TODO с помощью next получать следующий элемент от итератора
 
 
